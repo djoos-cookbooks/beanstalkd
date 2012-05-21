bf75d56b50842acab7baafe14df08c6e95d51e3d
David Joos <david.joos@gmail.com>
2012-05-21 17:40:10 +0100

    Supported OS

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          ""
 description      "Installs/Configures chef-beanstalkd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports "ubuntu", "10.04"
