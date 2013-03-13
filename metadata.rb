7a00254d50aed3a1ef2477a60c800cd00e8924f6
David Joos <david.joos@gmail.com>
2013-03-13 18:44:57 +0000

    metadata attribute change(s) & copyright update(s)

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
+name             "beanstalkd"
 maintainer       "Escape Studios"
 maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures beanstalkd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
-supports "ubuntu"
-supports "debian"
-supports "centos"
-supports "redhat"
-supports "fedora"
-supports "scientific"
-supports "amazon"
+%w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
+supports os
+end
+
+recommends "php"
+recommends "python"
+
+recipe "beanstalkd", "Installs beanstalkd."
