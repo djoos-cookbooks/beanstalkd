a253ca0e795cbee107f0ba38b58b07e825f4e907
Guilhem Lettron <guilhem@lettron.fr>
2013-07-10 17:49:35 +0200

    Clean recipe in chef way use 2 spaces ...

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,33 +6,33 @@
 #
 
 package "beanstalkd" do
-	action :upgrade
+  action :upgrade
 end
 
 case node[:platform]
-	when "debian", "ubuntu"
-		template_path = "/etc/default/beanstalkd" #templates/ubuntu
-	else
-		template_path = "/etc/sysconfig/beanstalkd" #templates/default
+when "debian", "ubuntu"
+  template_path = "/etc/default/beanstalkd" #templates/ubuntu
+else
+  template_path = "/etc/sysconfig/beanstalkd" #templates/default
 end
 
 template "#{template_path}" do
-	source "beanstalkd.erb"
-	owner "root"
-	group "root"
-	mode 0640
-	variables(
-		:opts => node[:beanstalkd][:opts],
-		:start_during_boot => node[:beanstalkd][:start_during_boot]
-	)
-	notifies :restart, "service[beanstalkd]"
+  source "beanstalkd.erb"
+  owner "root"
+  group "root"
+  mode 0640
+  variables(
+    :opts => node[:beanstalkd][:opts],
+    :start_during_boot => node[:beanstalkd][:start_during_boot]
+  )
+  notifies :restart, "service[beanstalkd]"
 end
 
 service "beanstalkd" do
-	start_command "/etc/init.d/beanstalkd start"
-	stop_command "/etc/init.d/beanstalkd stop"
-	status_command "/etc/init.d/beanstalkd status"
-	supports [:start, :stop, :status]
-    #starts the service if it's not running and enables it to start at system boot time
-	action [:enable, :start]
+  start_command "/etc/init.d/beanstalkd start"
+  stop_command "/etc/init.d/beanstalkd stop"
+  status_command "/etc/init.d/beanstalkd status"
+  supports [:start, :stop, :status]
+  #starts the service if it's not running and enables it to start at system boot time
+  action [:enable, :start]
 end
