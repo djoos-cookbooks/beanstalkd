d366a5a8266f2b423ec96ef9a63408f729fa1473
pwelch <paul@pwelch.net>
2015-09-19 16:47:48 -0400

    Improve ChefSpec Coverage
    This improves basic ChefSpec coverage.
    
    Using the ChefSpec Coverage Report causes it to see a
    yum_package[beanstalkd] when adding the CentOS platform to the unit
    tests. The CentOS platform is added to test the creation of 'other'
    /etc/beanstalkd file. Yum support will need to be added later if
    required.

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,5 +1,55 @@
 require 'spec_helper'
 
 describe 'beanstalkd::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  context 'When all attributes are default, on debian platform' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(:platform => 'ubuntu', :version => 14.04)
+        .converge(described_recipe)
+    end
+
+    it 'installs beanstalkd package' do
+      expect(chef_run).to upgrade_package('beanstalkd')
+    end
+
+    it 'creates /etc/default/beanstalkd.erb template' do
+      expect(chef_run).to create_template('/etc/default/beanstalkd').with(
+        :source => 'beanstalkd.erb',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => 0644
+      )
+    end
+
+    it 'enables the beanstalkd service' do
+      expect(chef_run).to enable_service('beanstalkd').with(
+        :start_command  => '/etc/init.d/beanstalkd start',
+        :stop_command   => '/etc/init.d/beanstalkd stop',
+        :status_command => '/etc/init.d/beanstalkd status'
+      )
+    end
+
+    it 'starts the beanstalkd service' do
+      expect(chef_run).to start_service('beanstalkd').with(
+        :start_command  => '/etc/init.d/beanstalkd start',
+        :stop_command   => '/etc/init.d/beanstalkd stop',
+        :status_command => '/etc/init.d/beanstalkd status'
+      )
+    end
+  end
+
+  let(:chef_run_other) do
+    ChefSpec::ServerRunner.new(:platform => 'centos', :version => 7.0)
+      .converge(described_recipe)
+  end
+
+  context 'When all attributes are default, on other linux platforms' do
+    it 'creates /etc/sysconfig/beanstalkd.erb template' do
+      expect(chef_run_other).to create_template('/etc/sysconfig/beanstalkd').with(
+        :source => 'beanstalkd.erb',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => 0644
+      )
+    end
+  end
 end
