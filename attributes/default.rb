115204d819376529538946c788eca2edcd8dab77
Michael <micmic.suarez@gmail.com>
2016-12-14 02:48:50 +0800

    add default values for opts
    Signed-off-by: Michael <micmic.suarez@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,10 @@
 # Copyright (c) 2016, David Joos
 #
 
-default['beanstalkd']['opts'] = {}
+default['beanstalkd']['opts'] = {
+	:p => '11300',
+ 	 :l => '127.0.0.1'
+}
 
 # Ubuntu only
 default['beanstalkd']['start_during_boot'] = false
