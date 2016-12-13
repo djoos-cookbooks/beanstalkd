78704662342e27d453007088cd48d424e735b3ee
Michael <micmic.suarez@gmail.com>
2016-12-14 02:51:34 +0800

    modified the beanstalkd service configuration
    Signed-off-by: Michael <micmic.suarez@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,33 +6,32 @@
 #
 
 package 'beanstalkd' do
-  action :upgrade
+	action :upgrade
 end
 
 case node['platform']
 when 'debian', 'ubuntu'
-  template_path = '/etc/default/beanstalkd' # templates/ubuntu
+	template_path = '/etc/default/beanstalkd' # templates/ubuntu
 else
-  template_path = '/etc/sysconfig/beanstalkd' # templates/default
+	template_path = '/etc/sysconfig/beanstalkd' # templates/default
 end
 
 template template_path do
-  source 'beanstalkd.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-  variables(
-    :opts => node['beanstalkd']['opts'],
-    :start_during_boot => node['beanstalkd']['start_during_boot']
-  )
-  notifies :restart, 'service[beanstalkd]'
+	source 'beanstalkd.erb'
+	owner 'root'
+	group 'root'
+	mode 0644
+	variables(
+		:opts => node['beanstalkd']['opts'],
+		:start_during_boot => node['beanstalkd']['start_during_boot']
+	)
+	notifies :restart, 'service[beanstalkd]'
 end
 
+
 service 'beanstalkd' do
-  start_command '/etc/init.d/beanstalkd start'
-  stop_command '/etc/init.d/beanstalkd stop'
-  status_command '/etc/init.d/beanstalkd status'
-  supports [:start, :stop, :status]
-  # starts the service if it's not running and enables it to start at system boot time
-  action [:enable, :start]
+	supports [:start, :stop, :status]
+	# starts the service if it's not running and enables it to start at system boot time
+	action [:enable, :start]
 end
+
