51d70bdfd8ff8eb6cb2464e08de0bc43ab46a874
Colin McFadden <colin@divergentmedia.com>
2019-04-08 18:42:17 +0600

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures beanstalkd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.3.2'
 
 %w(debian ubuntu redhat centos fedora scientific amazon).each do |os|
   supports os
