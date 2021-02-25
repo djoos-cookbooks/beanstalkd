2f2353e7427460981e8f962cb5a01698a05509d0
David Joos <david.joos@gmail.com>
2021-02-25 16:05:37 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'David Joos'
 maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures beanstalkd'
-version '0.4.0'
+version '0.4.1'
 
 %w(debian ubuntu redhat centos fedora scientific amazon).each do |os|
   supports os
